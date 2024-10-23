2a8384d28f4ee8847b1e7592fcc68d628b9309f4
SangNguyenSVN <nguyenvusang1108@gmail.com>
2024-10-23 21:22:45 +0700

    'sang'

--- app/src/services/authService.tsx
+++ app/src/services/authService.tsx
@@ -8,35 +8,6 @@ interface RegisterResponse {
         id: string;
         username: string;
         phoneNumber: string;
-        roleId: {
-            id: string;
-            name: string;
-            permissions: string[];
-        };
-    };
-}
-
-<<<<<<< HEAD
-
-=======
-interface LoginResponse {
-  
-    token: string;
-<<<<<<< HEAD
-   
-}
-
-const API_URL = 'http://192.168.1.13:3000/auth'; // Đổi thành URL thực tế của bạn
-=======
-    user: {
-        id: string;
-        username: string;
-        phoneNumber: string;
-        email: string;
-        gender: string;
-        dateOfBirth: Date;
-        fullname: string;
-        address: string;
         role: {
             id: string;
             name: string;
@@ -45,9 +16,7 @@ const API_URL = 'http://192.168.1.13:3000/auth'; // Đổi thành URL thực t�
     };
 }
 
-const API_URL = 'http://192.168.1.4:3000/api/auth'; // Đổi thành URL thực tế của bạn
->>>>>>> f4489b7af3607beec18bf2ba8d7c565354a2d687
->>>>>>> f031cfe18ce236131c7db6e19772888c54d204ac
+
 
 // Hàm đăng ký chung
 const registerUser = async (type: 'patient' | 'doctor', username: string, password: string, phoneNumber: string, roleId: string): Promise<RegisterResponse> => {
@@ -68,27 +37,9 @@ const registerDoctor = (username: string, password: string, phoneNumber: string,
     registerUser('doctor', username, password, phoneNumber, roleId);
 
 // Đăng nhập
-<<<<<<< HEAD
 const login = async (username: string, password: string, userType: string) => {
     try {
         const response = await apiClient.post('/auth/login', { username, password, userType });
-=======
-const login = async (username: String, password: string, roleId: string): Promise<LoginResponse | undefined> => {
-    try {
-<<<<<<< HEAD
-        console.log('Attempting to log in with:' + roleId);
-        
-        const response: AxiosResponse<LoginResponse> = await axios.post(`${API_URL}/login`, {
-            username,
-            password,
-            roleId
-        
-        });
-        
-        return response.data; // Return response if successful
-=======
-        const response: AxiosResponse<LoginResponse> = await axios.post(`${API_URL}/login`, { username, password });
->>>>>>> f031cfe18ce236131c7db6e19772888c54d204ac
 
         // Lưu user và token vào AsyncStorage
         await AsyncStorage.multiSet([
@@ -97,40 +48,9 @@ const login = async (username: String, password: string, roleId: string): Promis
         ]);
 
         return response.data;
-<<<<<<< HEAD
     } catch (error: any) {
         console.error('Lỗi đăng nhập:', error.response?.data);
-=======
->>>>>>> f4489b7af3607beec18bf2ba8d7c565354a2d687
-    } catch (error) {
-        if (axios.isAxiosError(error)) {
-            console.error('Lỗi đăng nhập:', error.response?.data);
-        } else {
-            console.error('Lỗi không xác định:', error);
-        }
->>>>>>> f031cfe18ce236131c7db6e19772888c54d204ac
     }
-<<<<<<< HEAD
-
-    return undefined; // Return undefined if there's an error
-};
-
-// Hàm để đăng nhập cho mọi người dùng thử api
-// const loginUser = async () => {
-//     try {
-//         const loginResponse = await login("test", "1","patient"); // Không cần truyền role
-//         console.log("Login Response:", loginResponse); // In toàn bộ phản hồi từ API
-
-//         if (loginResponse && loginResponse.user) {
-//             console.log("User role:", loginResponse.user.roleId); // Truy cập role từ user
-//         } else {
-//             console.error("Login response hoặc user là null hoặc undefined");
-//         }
-//     } catch (error) {
-//         console.error("Login error:", error);
-//     }
-// };
-=======
     return undefined;
 };
 
@@ -160,7 +80,6 @@ const updateAccount = async (password?: string, userType?: string): Promise<{ me
         throw new Error('Không thể thay đổi tài khoản');
     }
 };
->>>>>>> f4489b7af3607beec18bf2ba8d7c565354a2d687
 
 
 // Đăng xuất
@@ -179,9 +98,5 @@ export default {
     registerDoctor,
     login,
     logout,
-<<<<<<< HEAD
-    // loginUser
-=======
     updateAccount,
->>>>>>> f4489b7af3607beec18bf2ba8d7c565354a2d687
 };
